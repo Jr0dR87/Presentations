549eea7482020833cef584447f702175a145f057
Jarrod <me@jarrodrizor.com>
2026-07-19 17:55:33 -0400

    Update jailbreak_list_methods.js
    Added ObjC check.

--- Leveraging Frida to Bypass Mobile Application Security Controls/jailbreak_list_methods.js
+++ Leveraging Frida to Bypass Mobile Application Security Controls/jailbreak_list_methods.js
@@ -1,9 +1,12 @@
 if (ObjC.available) {
-    var className = "IOSSecuritySuite.JailbreakChecker"; // Because we found this from jailbreak_class_enum.js
-    var cls = ObjC.classes[className];
- 
+    console.log("[+] Successfully Connected");
+    const className = "IOSSecuritySuite.JailbreakChecker";
+    const cls = ObjC.classes[className];
     console.log("[*] Methods on " + className + ":");
     cls.$methods.forEach(function (m) {
         console.log("    " + m);
     });
+}
+else{
+    console.log("[-] ObjC not available");
 }
