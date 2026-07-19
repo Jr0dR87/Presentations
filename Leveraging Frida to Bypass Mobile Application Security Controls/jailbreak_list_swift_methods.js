bedd7926a8f468fb5ad416118c8352f6076d394b
Jarrod <me@jarrodrizor.com>
2026-07-19 17:57:17 -0400

    Update jailbreak_list_swift_methods.js
    remove debug app and put in release app (darkbank)

--- Leveraging Frida to Bypass Mobile Application Security Controls/jailbreak_list_swift_methods.js
+++ Leveraging Frida to Bypass Mobile Application Security Controls/jailbreak_list_swift_methods.js
@@ -1,14 +1,11 @@
 if (Swift.available){
-    console.log("[+] Successfully Connected");
-
-    const mod = Process.getModuleByName("DarkBank.debug.dylib");
+    const mod = Process.getModuleByName("DarkBank");
     const matches = mod.enumerateSymbols().filter(function (s) {
-        return s.name.indexOf("JailbreakChecker");
+        return s.name.indexOf("amIJailbroken") !== -1;
     });
-    
     console.log("[*] " + matches.length + " symbol(s) found:\n");
     matches.forEach(function (s) {
-        console.log(s.address + "  " + s.name);
+        console.log(s.address + " " + s.name);
     });
 }
 else{
