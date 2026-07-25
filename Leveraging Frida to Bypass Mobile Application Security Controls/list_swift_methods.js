e7c3be4f639ba520a154e7c7d3319a0ea13f23cb
Jarrod <me@jarrodrizor.com>
2026-07-25 16:28:12 -0400

    Update and rename jailbreak_list_swift_methods.js to list_swift_methods.js
    refactored

---
+++ Leveraging Frida to Bypass Mobile Application Security Controls/list_swift_methods.js
@@ -0,0 +1,16 @@
+if (Swift.available) {
+    console.log("[+] Successfully Connected and Detected Swift");
+    Process.enumerateModules().forEach(function (mod) {
+        const matches = mod.enumerateSymbols().filter(function (symbol) {
+            return symbol.name.includes("IOSSecuritySuite");
+        });
+        if (matches.length > 0) {
+            console.log("[*] Found: " + matches.length + " match(es) in: " + mod.name);
+            matches.forEach(function (symbol) {
+                console.log("Symbol Address: " + symbol.address + " Symbol Name: " + symbol.name);
+            });
+        }
+    });
+} else {
+    console.log("[-] Swift Not Available");
+}
