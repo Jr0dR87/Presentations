a3a6949ba130879ea07cbc17c4942082504f743f
Jarrod <me@jarrodrizor.com>
2026-07-28 10:51:42 -0400

    Update list_swift_methods.js
    Updating this because the last script was using legacy api.

--- Leveraging Frida to Bypass Mobile Application Security Controls/list_swift_methods.js
+++ Leveraging Frida to Bypass Mobile Application Security Controls/list_swift_methods.js
@@ -1,13 +1,17 @@
 if (Swift.available) {
     console.log("[+] Successfully Connected and Detected Swift");
-    Process.enumerateModules().forEach(function (mod) {
+    const moduleMap = new ModuleMap();
+    moduleMap.values().forEach(function (mod) {
         const matches = mod.enumerateSymbols().filter(function (symbol) {
             return symbol.name.includes("IOSSecuritySuite");
         });
         if (matches.length > 0) {
             console.log("[*] Found: " + matches.length + " match(es) in: " + mod.name);
             matches.forEach(function (symbol) {
-                console.log("Symbol Address: " + symbol.address + " Symbol Name: " + symbol.name);
+                console.log(
+                    "Symbol Address: " + symbol.address +
+                    " Symbol Name: " + symbol.name
+                );
             });
         }
     });
