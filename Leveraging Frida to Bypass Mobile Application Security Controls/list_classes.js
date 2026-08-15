2b42a1e0a2efd9dc604d83d0ac39034ee3751773
Jarrod <me@jarrodrizor.com>
2026-08-15 12:08:54 -0400

    Delete Leveraging Frida to Bypass Mobile Application Security Controls/list_classes.js
    removing script.

--- Leveraging Frida to Bypass Mobile Application Security Controls/list_classes.js
+++
@@ -1,9 +0,0 @@
-if(ObjC.available){
-    console.log("[+] Successfully Connected and Detected Objective-C");
-    for (const className in ObjC.classes){
-        console.log(className);
-    }
-}
-else{
-    console.log("[-] Objective-C not available");
-}
